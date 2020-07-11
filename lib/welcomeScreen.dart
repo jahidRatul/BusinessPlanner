cacc606880fd84bc31e8de822f503dc52750918c
jahidRatul <jahidratul@jahidRatuls-MacBook-Pro.local>
2020-07-12 05:44:17 +0600

    login done with formkey validator and api success code

--- lib/welcomeScreen.dart
+++ lib/welcomeScreen.dart
@@ -11,9 +11,19 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
   TextEditingController mobileController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  String messageValidation = '';
+
+  @override
+  void dispose() {
+    mobileController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,128 +38,139 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 ),
               ),
             ),
-            ListView(
-              children: <Widget>[
-                SizedBox(
-                  height: 70,
-                ),
-                Container(
-                  margin: EdgeInsets.all(30),
-                  child: Text(
-                    'Focus Productivity & Business Well..\n Welcome to Business Planner ',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.blueGrey),
+            Form(
+              key: _formKey,
+              child: ListView(
+                children: <Widget>[
+                  SizedBox(
+                    height: 70,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Card(
-                  color: Colors.transparent,
-                  margin: EdgeInsets.only(left: 30, top: 30, right: 30),
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(40),
-                    ),
-                  ),
-                  child: TextField(
-                    controller: mobileController,
-                    keyboardType: TextInputType.phone,
-                    style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    cursorColor: Color(0xFF9b9b9b),
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.phone_android,
-                        color: Colors.cyanAccent,
-                      ),
-                      hintText: 'Input Mobile Number',
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                      ),
-                    ),
-                  ),
-                ),
-                Card(
-                  color: Colors.transparent,
-                  margin: EdgeInsets.only(left: 30, top: 30, right: 30),
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(40),
-                    ),
-                  ),
-                  child: TextField(
-                    controller: passwordController,
-                    style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    cursorColor: Color(0xFF9b9b9b),
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.vpn_key, color: Colors.cyanAccent),
-                      hintText: 'Input Password',
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  margin: const EdgeInsets.only(left: 30, top: 30, right: 30),
-                  child: RaisedButton(
-                    elevation: 10,
-                    color: Colors.pink,
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40))),
-                    onPressed: _isLoading ? null : _login,
+                  Container(
+                    margin: EdgeInsets.all(30),
                     child: Text(
-                      _isLoading ? 'Logging...' : 'Login',
+                      'Focus Productivity & Business Well..\n Welcome to Business Planner ',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.blueGrey),
                     ),
-                    disabledColor: Colors.grey,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Don\'t have account? ',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, RegisterScreen.id);
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: mobileController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Number cannot be empty';
+                        }
+                        return null;
                       },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(fontSize: 17),
+                      keyboardType: TextInputType.phone,
+                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                      cursorColor: Color(0xFF9b9b9b),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.phone_android,
+                          color: Colors.cyanAccent,
+                        ),
+                        hintText: 'Input Mobile Number',
+                        hintStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.indigo),
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(40),
+                          ),
+                        ),
                       ),
-                      textColor: Colors.indigo,
-                    )
-                  ],
-                )
-              ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: passwordController,
+                      validator: (value) {
+                        if (value.isEmpty) return 'Password cannot be empty';
+                        return null;
+                      },
+                      obscureText: true,
+                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                      cursorColor: Color(0xFF9b9b9b),
+                      decoration: InputDecoration(
+                        prefixIcon:
+                            Icon(Icons.vpn_key, color: Colors.cyanAccent),
+                        hintText: 'Input Password',
+                        hintStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.indigo),
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(left: 30, top: 30, right: 30),
+                    child: RaisedButton(
+                      elevation: 10,
+                      color: Colors.pink,
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                      ),
+                      onPressed: _login,
+                      child: Text(
+                        _isLoading ? 'Logging...' : 'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    messageValidation,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Don\'t have account? ',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, RegisterScreen.id);
+                        },
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        textColor: Colors.indigo,
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
@@ -158,36 +179,44 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   }
 
   void _login() async {
-    setState(() {
-      _isLoading = true;
-    });
+    // form key is for validator in textformfield
+    if (_formKey.currentState.validate()) {
+      setState(() {
+        messageValidation = '';
+        _isLoading = true;
+      });
 
-    // if (android emulator + local server) thn should be 10.0.2.2:5000 instead of localhost:5000
-    final url = 'http://10.0.2.2:5000/api/users/login';
+      // if (android emulator + local server) thn should be 10.0.2.2:5000 instead of localhost:5000
+      final url = 'http://10.0.2.2:5000/api/users/login';
 
-    Map data = {
-      'mobileno': mobileController.text,
-      'password': passwordController.text
-    };
+      Map data = {
+        'mobileno': mobileController.text,
+        'password': passwordController.text
+      };
 
-    //encode Map to JSON
-    var bodyValue = json.encode(data);
+      //encode Map to JSON
+      var bodyValue = json.encode(data);
 
-    http.Response response = await http.post(url,
-        headers: {"Content-Type": "application/json"}, body: bodyValue);
+      http.Response response = await http.post(url,
+          headers: {"Content-Type": "application/json"}, body: bodyValue);
 
-    print(response.body);
+      print(response.body);
 
-    //returning as text
-    Map<String, dynamic> user = jsonDecode(response.body);
-    print("access token -> " + user['token']);
-    print("accId -> " + user['accId'].toString());
-    print("name -> " + user['name']);
+      //returning as text in map
+      Map<String, dynamic> user = jsonDecode(response.body);
+      print("success msg -> " + user['message']);
 
-    Navigator.pushNamed(context, HomeScreen.id);
+      if (user['success'] == 1) {
+        Navigator.pushNamed(context, HomeScreen.id);
+      } else {
+        setState(() {
+          messageValidation = user['message'];
+        });
+      }
 
-    setState(() {
-      _isLoading = false;
-    });
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 }
