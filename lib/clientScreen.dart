4ac2bac91b89d9d784be4b3b015fcb6ee755ee36
jahidRatul <jahidratul@jahidRatuls-MacBook-Pro.local>
2020-07-14 03:17:14 +0600

    adding new client by accessing token works with UI

--- lib/clientScreen.dart
+++ lib/clientScreen.dart
@@ -1,88 +1,126 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'officeScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
-class ClientScreen extends StatelessWidget {
+class ClientScreen extends StatefulWidget {
   static String id = 'client';
 
   @override
-  Widget build(BuildContext context) {
-    return
-      Scaffold(
-        appBar: AppBar(
-          title: Text(" Business Planner "),
-          centerTitle: true,
-        ),
-        body: ClientWidget(),
-      );
-  }
+  _ClientScreenState createState() => _ClientScreenState();
 }
 
-class ClientWidget extends StatelessWidget {
+class _ClientScreenState extends State<ClientScreen> {
+  TextEditingController clientController = TextEditingController();
+  var userKey;
+  var userId;
+
+  @override
+  void dispose() {
+    clientController.dispose();
+    super.dispose();
+  }
+
+  void initState() {
+    _getUserInfo();
+
+    super.initState();
+  }
+
+  _getUserInfo() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    String keyValue = localStorage.getString('accessKey');
+    String nameValue = localStorage.getString('userName');
+    int uidValue = localStorage.getInt('uId');
+    userKey = keyValue;
+    userId = uidValue;
+
+    print(keyValue);
+    print(nameValue);
+    print(uidValue);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        SizedBox(height: 30),
-
-        RaisedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, OfficeScreen.id);
-          },
-          textColor: Colors.white,
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            width: double.infinity,
-
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: <Color>[
-                  Color(0xFF0D47A1),
-                  Color(0xFF1976D2),
-                  Color(0xFF42A5F5),
-                ],
-              ),
-            ),
-            padding: EdgeInsets.all(15.0),
-            child: Text('Cash Pay', style: TextStyle(fontSize: 20)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" Business Planner "),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                color: Colors.white70,
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      SizedBox(height: 20),
+                      Text(
+                        'Add Client',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.indigo),
+                      ),
+                      TextField(
+                        autofocus: true,
+                        textAlign: TextAlign.center,
+                        controller: clientController,
+                      ),
+                      RaisedButton(
+                        child: const Text(
+                          'Submit',
+                          style: TextStyle(fontSize: 22, color: Colors.white),
+                        ),
+                        onPressed: _addClient,
+                        color: Colors.indigo,
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        label: Text(
+          'Add Client',
+          style: TextStyle(
+            fontSize: 18,
           ),
         ),
-        SizedBox(height: 30),
-        RaisedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, OfficeScreen.id);
-          },
-          textColor: Colors.white,
-          padding: EdgeInsets.all(15.0),
-          child: Container(
-            width: double.infinity,
-
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: <Color>[
-                  Color(0xFF0D47A1),
-                  Color(0xFF1976D2),
-                  Color(0xFF42A5F5),
-                ],
-              ),
-            ),
-            padding: EdgeInsets.all(15.0),
-            child: Text('Bank Pay', style: TextStyle(fontSize: 20)),
-          ),
-        ),
-        SizedBox(height: 30),
-
-      ],
+        icon: Icon(Icons.add),
+        backgroundColor: Colors.indigo,
+      ),
     );
   }
+
+  _addClient() async {
+    final url = 'http://10.0.2.2:5000/api/clients/client';
+
+    if (clientController.text.isNotEmpty) {
+      Map data = {'name': clientController.text, 'uId': userId};
+      //encode Map to JSON
+      var bodyValue = json.encode(data);
+
+      http.Response response = await http.post(url,
+          headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer $userKey'
+          },
+          body: bodyValue);
+
+      print(response.body);
+
+      //returning as text in map
+      Map<String, dynamic> user = jsonDecode(response.body);
+      print("success msg -> " + user['message']);
+      clientController.clear();
+      Navigator.pop(context);
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
